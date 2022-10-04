47461f5bed0f101aec2e4f73a2d78dc22672c1f5
rafasanzvi <rafasv41@gmail.com>
2022-10-04 18:38:39 +0200

    Error message and functionality added

---
+++ js/app.js
@@ -0,0 +1,44 @@
+//Variables
+const form = document.querySelector("#formulario")
+const listTweets = document.querySelector("#lista-tweets")
+let tweets = [] //This variable saves all tweets
+
+//Event listeners
+eventListeners()
+
+function eventListeners() {
+    form.addEventListener("submit", addTweets)
+}
+
+
+//Functions
+function addTweets(e) {
+    e.preventDefault()
+
+    const individualTweet = document.querySelector("#tweet").value
+
+    //Validating
+    if (individualTweet === "") {
+        showError("This space cannot be empty")
+
+        return //Prevents the further use of additional lines of code
+    }
+
+    //Add to tweets array
+    
+}
+
+function showError(error) { 
+    const messageError = document.createElement("p")
+    messageError.textContent = error
+    messageError.classList.add("error")
+
+    //Insert in the content
+    const content = document.querySelector("#contenido")
+    content.appendChild(messageError)
+
+    //To remove 
+    setTimeout(() => {
+        messageError.remove()
+    },3000)
+}
