27e1d9551050fd1109815660cc484c99c087c530
rafasanzvi <rafasv41@gmail.com>
2022-10-05 20:16:52 +0200

    Delete button and local storage added

--- js/app.js
+++ js/app.js
@@ -7,7 +7,17 @@ let tweets = [] //This variable saves all tweets
 eventListeners()
 
 function eventListeners() {
+    //When the user adds a new tweet
     form.addEventListener("submit", addTweets)
+
+    //When the document is ready
+    document.addEventListener("DOMContentLoaded", () => {
+        tweets = JSON.parse( localStorage.getItem("tweets") ) || []
+
+        console.log(tweets)
+
+        createHtml()
+    })
 }
 
 
@@ -62,21 +72,49 @@ function createHtml() {
     if (tweets.length > 0) {
 
         tweets.forEach(tweet => {
+            //Add a botton to remove
+            const btnRemove = document.createElement("a")
+            btnRemove.classList.add("borrar-tweet")
+            btnRemove.innerText = "X"
+
+            //Add the function of delete
+            btnRemove.onclick = () => {
+                deleteTweet(tweet.id)
+            }
 
             //Create HTML
             const li = document.createElement("li")
 
             //add text
             li.innerText = tweet.individualTweet
+            li.appendChild(btnRemove)
 
             //insert in HTML
             listTweets.appendChild(li)
         })
     }
+
+    synchroniseLocalStorage()
 }
+
+//Add the currently tweets to Local Storage
+function synchroniseLocalStorage() {
+    localStorage.setItem("tweets", JSON.stringify(tweets))
+}
+
+//Delete a tweet
+function deleteTweet(tweetToDelete) {
+    tweets = tweets.filter(tweets => tweets.id !== tweetToDelete)
+
+    createHtml()
+}
+
 
 function cleanHTML() {
     while (listTweets.firstChild) {
         listTweets.removeChild(listTweets.firstChild)
     }
 }
+
+
+
